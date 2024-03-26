2686199c7fc0441de95270970ccefdd08fd5d6ad
Npwskp <nopawit.sukolpuk@gmail.com>
2024-03-26 17:28:30 +0700

    itnv

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -8,18 +8,22 @@ contract RPS is CommitReveal {
     struct Player {
         uint choice; // 0 - Rock, 1 - Water, 2 - Air , 3 - Paper , 4 - Sponge , 5 - Scissor, 6 - Fire, 7 - undefined
         address addr;
+        uint point;
     }
 
     uint public numPlayer = 0;
     uint public reward = 0;
     mapping (uint => Player) public player;
     mapping (address => uint) private idx;
+    mapping (uint => bool) public Revealed;
     uint public numInput = 0;
     uint public playerReveal = 0;
     uint public startTime = 0;
+    uint private maxPoint = 0;
+    uint private maxCount = 0;
 
     function addPlayer() public payable {
-        require(numPlayer < 2);
+        require(numPlayer < 3);
         require(msg.value == 1 ether);
         reward += msg.value;
         player[numPlayer].addr = msg.sender;
@@ -32,7 +36,7 @@ contract RPS is CommitReveal {
     }
 
     function input(bytes32 choice) public  {
-        require(numPlayer == 2);
+        require(numPlayer == 3);
         commit(choice);
         numInput++;
     }
@@ -44,11 +48,12 @@ contract RPS is CommitReveal {
     }
 
     function revealByUser(uint choice, uint salt) public {
-        require(numInput == 2);
+        require(numInput == 3);
         revealAnswer(bytes32(choice),bytes32(salt));
         player[idx[msg.sender]].choice = choice;
+        Revealed[idx[msg.sender]] = true;
         playerReveal++;
-        if(playerReveal == 2){
+        if(playerReveal == 3){
             _checkWinnerAndPay();
         }
     }
@@ -61,50 +66,70 @@ contract RPS is CommitReveal {
     } 
 
     function _handleOvertime() private {
-        if(numPlayer == 1){
-            // in case only 1 player
-            address payable account0 = payable(player[0].addr);
-            account0.transfer(reward);
-        }
-        else if (numPlayer == 2){
-            // only punish who is not reveal
-            uint p0Choice = player[0].choice;
-            uint p1Choice = player[1].choice;
-            address payable account0 = payable(player[0].addr);
-            address payable account1 = payable(player[1].addr);
-
-            //transfer reward to those who already reveal
-            if(p0Choice == 7 && p1Choice != 7 && playerReveal < 2){
-                account1.transfer(reward);
-            }
-            else if(p0Choice != 7 && p1Choice == 7 && playerReveal < 2){
-                account0.transfer(reward);
-            }
-            else {
-                // in case that both are not reveal or only 1 person commit
-                account0.transfer(reward / 2);
-                account1.transfer(reward / 2);
+        require(block.timestamp > startTime + 1 days, "Not overtime yet");
+        if (playerReveal != 0){ 
+            uint amount = reward/playerReveal;
+            for(uint i=0; i<numPlayer ; i++){
+                if(Revealed[i] == true){
+                    address payable playerAddress = payable (player[i].addr);
+                    reward -= amount;
+                    playerAddress.transfer(amount);
+                }
+            _resetGame();
             }
         }
     }
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = player[0].choice;
-        uint p1Choice = player[1].choice;
-        address payable account0 = payable(player[0].addr);
-        address payable account1 = payable(player[1].addr);
-        if ((p0Choice + 1) % 7 == p1Choice || (p0Choice + 2) % 7 == p1Choice  || (p0Choice + 3) % 7 == p1Choice ) {
-            // to pay player[1]
-            account1.transfer(reward);
+        for(uint i=0; i<numPlayer ; i++){
+            uint ci = player[i].choice;
+            for(uint j=0; i<numPlayer ; j++){
+                uint cj = player[j].choice;
+                if(i != j){
+                    if ((ci + 1) % 7 == cj || (ci + 2) % 7 == cj  || (ci + 3) % 7 == cj ) {
+                        // to pay player[1]
+                    }
+                    else if ((cj + 1) % 7 == ci || (cj + 2) % 7 == ci || (cj + 3) % 7 == ci) {
+                        // to pay player[0]
+                        player[i].point += 3;
+                    }
+                    else {
+                        player[i].point += 1;
+                    }
+                }
+            }
+            if(player[i].point > maxPoint){
+                maxPoint = player[i].point;
+                maxCount = 1;
+            }
+            else if(player[i].point > maxPoint){
+                maxCount += 1;
+            }
         }
-        else if ((p1Choice + 1) % 7 == p0Choice || (p1Choice + 2) % 7 == p0Choice || (p1Choice + 3) % 7 == p0Choice) {
-            // to pay player[0]
-            account0.transfer(reward);    
+
+        uint pay = reward/maxCount;
+        for(uint i=0; i<numPlayer ; i++){
+            if(player[i].point == maxPoint && reward != 0){
+                reward -= pay;
+                (payable(player[i].addr)).transfer(pay);
+            }
         }
-        else {
-            // to split reward
-            account0.transfer(reward / 2);
-            account1.transfer(reward / 2);
+
+
+
+    }
+
+    function _resetGame() private {
+        numPlayer = 0;
+        reward = 0;
+        numInput = 0;
+        playerReveal = 0;
+        startTime = 0;
+        for(uint i=0; i<numPlayer ; i++){
+            player[i].addr = address(0);
+            player[i].choice = 7;
+            player[i].point = 0;
+            Revealed[i] = false;
         }
     }
 }
